2f45c2abe5c58a251f5abf57cb0190cd67e655d1
Reyemneirda <reyemneirda@gmail.com>
2025-07-24 15:44:19 +0300

    Remove Search icon from GameComparison for nicer design

--- apps/frontend/src/components/GameComparison.tsx
+++ apps/frontend/src/components/GameComparison.tsx
@@ -210,7 +210,6 @@ export const GameComparison = () => {
         
         <div className="relative mb-6">
           <div className="relative">
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400 w-4 h-4" />
             <input
               type="text"
               placeholder={`Search for ${selectedGames.length === 0 ? 'first' : 'second'} game...`}
